e82b367b8418d83377c4a4f025e3ab39887e0984
SretoSV <sreto.vujicic@gmail.com>
2025-07-19 18:08:20 +0200

    Setup react router

--- web-react-frontend/src/main.tsx
+++ web-react-frontend/src/main.tsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
+import { LoginPage } from './pages/LoginPage.tsx';
+import { RegisterPage } from './pages/RegisterPage.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LoginPage />
+  },
+  {
+    path: "/Login",
+    element: <LoginPage />
+  },
+  {
+    path: "/Register",
+    element: <RegisterPage />
+  },
+  {
+    path: "*",
+    element: <Navigate to="/" replace />
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
+  <RouterProvider router={router} />,
+)
+
+/*
   <StrictMode>
     <App />
   </StrictMode>,
-)
+*/
